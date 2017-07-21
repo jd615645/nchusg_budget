929f9b03efb2057ec8580d5fcb7703caf74913c4
jd615645 <jd615645@gmail.com>
2017-07-21 18:49:43 +0800

    :cat2: add ga

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,67 @@
-var gulp = require('gulp');
-// var path = require('path');
-var $ = require('gulp-load-plugins')();
+var gulp = require('gulp')
+// var path = require('path')
+var $ = require('gulp-load-plugins')()
 
 var paths = {
   src: {
-    less  : './src/style/*.less',
-    js    : './src/js/*.js',
-    pug   : './src/pug/*.pug',
+    less: './src/style/*.less',
+    js: './src/js/*.js',
+    pug: './src/pug/*.pug',
     images: './src/img/**',
     data: './src/data/**'
   },
   dist: {
-    html  : './dist',
-    css   : './dist/styles',
-    js    : './dist/js',
+    html: './dist',
+    css: './dist/styles',
+    js: './dist/js',
     images: './dist/img',
     data: './dist/data'
   }
 }
 
-gulp.task('pug', function() {
+gulp.task('pug', () => {
   gulp.src(paths.src.pug)
-      .pipe($.pug())
-      .pipe(gulp.dest('./dist'));
-});
+    .pipe($.pug())
+    .pipe(gulp.dest('./dist'))
+})
 
-gulp.task('less', function() {
+gulp.task('less', () => {
   gulp.src(paths.src.less)
-      .pipe($.less())
-      .pipe(gulp.dest(paths.dist.css));
-});
+    .pipe($.less())
+    .pipe(gulp.dest(paths.dist.css))
+})
 
-gulp.task('scripts', function() {
+gulp.task('scripts', () => {
   gulp.src(paths.src.js)
-      // .pipe($.uglify())
-      .pipe(gulp.dest(paths.dist.js))
+    // .pipe($.uglify())
+    .pipe(gulp.dest(paths.dist.js))
 })
 
-gulp.task('images', function() {
+gulp.task('images', () => {
   gulp.src(paths.src.images)
-      .pipe($.imagemin())
-      .pipe(gulp.dest(paths.dist.images))
+    .pipe($.imagemin())
+    .pipe(gulp.dest(paths.dist.images))
 })
 
-gulp.task('dataSrc', function(){
+gulp.task('dataSrc', () => {
   gulp.src(paths.src.data)
-      .pipe(gulp.dest(paths.dist.data))
+    .pipe(gulp.dest(paths.dist.data))
 })
 
-gulp.task('webserver', function(){
+gulp.task('webserver', () => {
   gulp.src(paths.dist.html)
-      .pipe($.webserver({
-        port: 8080,
-        livereload: true,
-        directoryListing: false
-      }));
+    .pipe($.webserver({
+      port: 8080,
+      livereload: true,
+      directoryListing: false
+    }))
 })
 
-gulp.task('watch',function(){
+gulp.task('watch', () => {
   gulp.watch(paths.src.pug, ['pug'])
-  gulp.watch(paths.src.less, ['less']);
-  gulp.watch(paths.src.js, ['scripts']);
-});
+  gulp.watch(paths.src.less, ['less'])
+  gulp.watch(paths.src.js, ['scripts'])
+})
 
-gulp.task('default', ['webserver', 'watch']);
-gulp.task('build', ['pug', 'less', 'scripts', 'dataSrc']);
+gulp.task('default', ['webserver', 'watch'])
+gulp.task('build', ['pug', 'less', 'scripts', 'dataSrc'])
